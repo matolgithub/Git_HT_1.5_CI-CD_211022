9899d991564746752da36afdd7843812728a90e9
matolgithub <89175955650@mail.ru>
2022-10-21 17:48:34 +0300

    First commit in CI/CD project.

---
+++ logistic/views.py
@@ -0,0 +1,25 @@
+from rest_framework.filters import OrderingFilter, SearchFilter
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['id', 'title']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['products']
+    search_fields = ['products__title', 'products__description']
+    ordering_fields = ['id', 'address', 'products']
